f235bb7edb9136a795bf12b4535d3638b3a0930d
pwaldeku <abiau.pro@outlook.fr>
2019-03-25 01:32:02 +0100

    + Design Cardview

--- app/src/main/java/com/example/users/simpsonsview/activities/SimpsonsActivity.java
+++ app/src/main/java/com/example/users/simpsonsview/activities/SimpsonsActivity.java
@@ -2,6 +2,7 @@ package com.example.users.simpsonsview.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.users.simpsonsview.R;
 
 public class SimpsonsActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class SimpsonsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simpsons);
+
+        getSupportActionBar().hide();
     }
 }
